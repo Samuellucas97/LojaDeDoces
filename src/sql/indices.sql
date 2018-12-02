5dd4433d60800882bc1f121ef03965d322cc37ee
Samuel Lucas <samuel1797@gmail.com>
2018-12-02 12:02:56 -0300

    Update indices.sql

--- src/sql/indices.sql
+++ src/sql/indices.sql
@@ -3,3 +3,6 @@ CREATE INDEX index_Cliente ON Cliente USING BTREE(idCliente);
 
 --Índices da tabela Funcionario
 CREATE INDEX index_Funcionario ON Funcionario USING HASH(cpf);
+
+--Índices da tabela Cargo
+CREATE INDEX index_Cargo ON Cargo USING HASH (nomeCargo);
