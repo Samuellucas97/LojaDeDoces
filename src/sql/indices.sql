73b0616206c945a77e41e2bb8a9d4607069a08a5
Samuel Lucas <samuel1797@gmail.com>
2018-12-02 12:19:35 -0300

    Update indices.sql

--- src/sql/indices.sql
+++ src/sql/indices.sql
@@ -1,8 +1,14 @@
---Índices da tabela Cliente
-CREATE INDEX index_Cliente ON Cliente USING BTREE(idCliente);
+-- Índices da tabela Cliente
+CREATE INDEX index_btree_Cliente ON Cliente USING BTREE(idCliente);
 
---Índices da tabela Funcionario
-CREATE INDEX index_Funcionario ON Funcionario USING HASH(cpf);
+-- Índices da tabela Telefone
+CREATE INDEX index_hash_Telefone ON Telefone USING HASH(numeroTelefone);
 
---Índices da tabela Cargo
-CREATE INDEX index_Cargo ON Cargo USING HASH (nomeCargo);
+-- Índices da tabela Funcionario
+CREATE INDEX index_hash_Funcionario ON Funcionario USING HASH(cpf);
+
+-- Índices da tabela Cargo
+CREATE INDEX index_Cargo ON Cargo USING HASH(nomeCargo);
+
+-- Índices da tabela Fabricante
+CREATE INDEX index_btree_Fabricante ON Fabricante USING BTREE(idFabricante);
